a564832dcb39bea4f8b8f339386fc84245270e5b
Julia Desmazes <julia.desmazes@gmail.com>
2026-07-21 16:05:15 -0500

    updating micropython tb

--- bringup/main.py
+++ bringup/main.py
@@ -1,30 +1,32 @@
-'''
+"""
 Created on Jan 9, 2024
 
 Code here, in main.py, runs on every power-up.
 
-You can put anything you like in here, including any utility functions 
-you might want to have access to when connecting to the REPL.  
+You can put anything you like in here, including any utility functions
+you might want to have access to when connecting to the REPL.
 
 If you want to use the SDK, all
 you really need is something like
-      
-      DemoboardDetect.probe()
-      tt = DemoBoard.get()
 
-Then you can 
-    # enable test project
-    tt.shuttle.tt_um_factory_test.enable()
+          DemoboardDetect.probe()
+          tt = DemoBoard.get()
+
+Then you can
+        # enable test project
+        tt.shuttle.tt_um_factory_test.enable()
 
 and play with i/o as desired.
 
 @author: Pat Deegan
 @copyright: Copyright (C) 2024 Pat Deegan, https://psychogenic.com
-'''
+"""
+
 print("BOOT: Tiny Tapeout SDK")
 import gc
+import test
 
-# stash the current value for garbage 
+# stash the current value for garbage
 # collection threshold (is -1/when full, by default)
 GCThreshold = gc.threshold()
 # start very aggressive, to keep thing defragged
@@ -32,9 +34,10 @@ GCThreshold = gc.threshold()
 gc.threshold(80000)
 
 import ttboard.log as logging
+
 # logging.ticksStart() # start-up tick delta counter
 
-logging.basicConfig(level=logging.DEBUG, filename='debug.log')
+logging.basicConfig(level=logging.DEBUG, filename="boot.log")
 
 
 import micropython
@@ -47,6 +50,8 @@ import ttboard.util.colors as colors
 gc.collect()
 
 tt = None
+
+
 def startup():
     # construct DemoBoard
     # either pass an appropriate RPMode, e.g. RPMode.ASIC_RP_CONTROL
@@ -55,43 +60,57 @@ def startup():
     print("\n\n")
     print(f"The '{colors.color('tt', 'red')}' object is available.")
     print()
-    print(f"Projects may be enabled with {colors.bold('tt.shuttle.PROJECT_NAME.enable()')}, e.g.")
+    print(
+        f"Projects may be enabled with {colors.bold('tt.shuttle.PROJECT_NAME.enable()')}, e.g."
+    )
     print("tt.shuttle.tt_um_urish_simon.enable()")
     print()
-    print(f"The io ports are named as in Verilog, {colors.bold('tt.ui_in')}, {colors.bold('tt.uo_out')}...")
-    print(f"and behave as with cocotb, e.g. {colors.bold('tt.uo_out.value = 0xAA')} or {colors.bold('print(tt.ui_in.value)')}")
-    print(f"Bits may be accessed by index, e.g. {colors.bold('tt.uo_out[7]')} (note: that's the {colors.color('high bit!', 'red')}) to read or {colors.bold('tt.ui_in[5] = 1')} to write.")
-    print(f"Direction of the bidir pins is set using {colors.bold('tt.uio_oe_pico')}, used in the same manner as the io ports themselves.")
+    print(
+        f"The io ports are named as in Verilog, {colors.bold('tt.ui_in')}, {colors.bold('tt.uo_out')}..."
+    )
+    print(
+        f"and behave as with cocotb, e.g. {colors.bold('tt.uo_out.value = 0xAA')} or {colors.bold('print(tt.ui_in.value)')}"
+    )
+    print(
+        f"Bits may be accessed by index, e.g. {colors.bold('tt.uo_out[7]')} (note: that's the {colors.color('high bit!', 'red')}) to read or {colors.bold('tt.ui_in[5] = 1')} to write."
+    )
+    print(
+        f"Direction of the bidir pins is set using {colors.bold('tt.uio_oe_pico')}, used in the same manner as the io ports themselves."
+    )
     print("\n")
     print(f"{colors.color('TT SDK v' + ttdemoboard.version, 'cyan')}")
     print("\n\n")
     gc.collect()
     return ttdemoboard
 
-def autoClockProject(freqHz:int):
+
+def autoClockProject(freqHz: int):
     tt.clock_project_PWM(freqHz)
-    
+
+
 def stopClocking():
     tt.clock_project_stop()
 
 
 # Detect the demoboard version
-detection_result = '(best guess)'
-detection_color = 'red'
+detection_result = "(best guess)"
+detection_color = "red"
 if DemoboardDetect.probe():
     # detection was conclusive
-    detection_result = ''
-    detection_color = 'cyan'
-detection_message = 'Detected ' + DemoboardDetect.PCB_str() + ' demoboard ' + detection_result
+    detection_result = ""
+    detection_color = "cyan"
+detection_message = (
+    "Detected " + DemoboardDetect.PCB_str() + " demoboard " + detection_result
+)
 print(f"{colors.color(detection_message, detection_color)}")
 
 
 tt = startup()
 
 
-logging.basicConfig(filename="config.ini")
+logging.basicConfig(filename=None)
 gc.collect()
-colors.color_start('magenta', False)
+colors.color_start("magenta", False)
 print("Mem info")
 micropython.mem_info()
 colors.color_end()
@@ -100,12 +119,33 @@ colors.color_end()
 print(tt)
 print()
 
-logging.dumpTicksMsDelta('boot done')
+logging.dumpTicksMsDelta("boot done")
 print(f"tt.sdk_revision={tt.revision}")
 print(f"tt.sdk_version={tt.version}")
 # end by being so aggressive on collection
 gc.threshold(GCThreshold)
 
-# to run tests easily import a module of interest, as below, and then 
-# run() it
 
+def enable_essen():
+    from ttboard.demoboard import DemoBoard, RPMode
+
+    tt = DemoBoard.get()
+    print("Got shuttle chip:")
+    print(tt.shuttle)
+    if not tt.shuttle.has("tt_um_essen"):
+        print("No tt_um_essen this shuttle - fail")
+        return
+
+    tt.shuttle.tt_um_essen.enable()
+    if tt.mode != RPMode.ASIC_RP_CONTROL:
+        print("Setting mode to ASIC_RP_CONTROL")
+        tt.mode = RPMode.ASIC_RP_CONTROL
+
+    # oe enable values
+    tt.uio_oe_pico.value = 0b11000000
+    print(f"oe enabled to: {tt.uio_oe_pico}")
+    print(f"tt_um_essen configured with options: {tt.shuttle.tt_um_essen}")
+
+
+enable_essen()
+test.start_cocotb(tt)
---
+++ bringup/test.py
@@ -0,0 +1,173 @@
+# Cocotb testbench for testing the MAC and JTAG functions of this ASIC design
+#
+# Julia Desmazes, 2025, human made code
+
+from microcotb.clock import Clock
+from microcotb.triggers import ClockCycles
+import microcotb as cocotb
+
+import random
+import mac_utils
+import asyncio
+from array import array
+
+N = 2  # matrix dimention
+
+# cover full range of i8
+MIN_W = -128
+MAX_W = 127
+MIN_I = -128
+MAX_I = 127
+
+
+def start_clk(dut):
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())  # runs the clock "in the background"
+
+
+# Reset sequence
+async def rst(dut):
+    dut.rst_n.value = 0
+    start_clk(dut)
+    await ClockCycles(dut.clk, 2)
+    # set default io
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
+
+
+def mac(W, I):
+    res = array("b", [0, 0, 0, 0])
+    assert len(W) == N * N
+    assert len(I) == N * N
+    for x in range(0, N):
+        for y in range(0, N):
+            for ix in range(0, N):
+                tmp = res[y * N + x] + I[y * N + ix] * W[ix * N + x]
+                if tmp >= 127:
+                    tmp = 127
+                if tmp <= -128:
+                    tmp = -128
+                res[y * N + x] = tmp
+    return res
+
+
+async def read_res(dut):
+    res = array("b")
+
+    while len(res) != N * N:
+        if dut.result_v.value == 1:
+            x = dut.uo_out.value.to_signed()
+            res.append(x)
+        await ClockCycles(dut.clk, 1)
+
+    return res
+
+
+async def compare_res(dut, W, I):
+    expected = mac(W, I)
+    res = await read_res(dut)
+
+    cocotb.log.info("expected vs got :")
+    cocotb.log.info(" ".join(map(str, expected)))
+    cocotb.log.info(" ".join(map(str, res)))
+
+    assert res == expected
+
+
+# MAC tests
+
+
+@cocotb.test()
+async def simple_mac_test(dut):
+    await rst(dut)
+    W = array("b", [0, 1, 2, 3])
+    I = array("b", [4, 5, 6, 7])
+
+    await mac_utils.rst_data_addr(dut)
+
+    # send weights
+    await mac_utils.write_config(dut, W, weight=True)
+
+    # res can start comming in before all the data has been finished being written
+    comp_task = cocotb.start_soon(compare_res(dut, W, I))
+
+    # send data
+    write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+
+    await write_task
+    await comp_task
+
+    await ClockCycles(dut.clk, 10)
+
+
+@cocotb.test()
+async def random_mac_test(dut):
+    await rst(dut)
+    await mac_utils.rst_data_addr(dut)
+    for _ in range(0, 500):
+        W = array("b")
+        I = array("b")
+        for _ in range(0, 4):
+            W.append(mac_utils.biased_random(MIN_W, MAX_W))
+            I.append(mac_utils.biased_random(MIN_I, MAX_I))
+
+        # send weights
+        await mac_utils.write_config(dut, W, weight=True)
+
+        # check result - results can start streaming before all the
+        # data has been written
+        comp_task = cocotb.start_soon(compare_res(dut, W, I))
+
+        # send data
+        write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+
+        await write_task
+        await comp_task
+
+
+@cocotb.test()
+async def random_mac_reuse_weights_test(dut):
+    await rst(dut)
+    await mac_utils.rst_data_addr(dut)
+    for _ in range(0, 20):
+        W = array("b")
+        for _ in range(0, 4):
+            W.append(mac_utils.biased_random(MIN_W, MAX_W))
+        await mac_utils.write_config(dut, W, weight=True)
+
+        for _ in range(0, 50):
+            I = array("b")
+            for _ in range(0, 4):
+                I.append(mac_utils.biased_random(MIN_I, MAX_I))
+
+            # check result - results can start streaming before all the
+            # data has been written
+            comp_task = cocotb.start_soon(compare_res(dut, W, I))
+            # write data
+            write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+
+            await write_task
+            await comp_task
+
+
+def start_cocotb(tt):
+    import ttboard.cocotb.dut
+
+    class DUT(ttboard.cocotb.dut.DUT):
+        def __init__(self):
+            super().__init__("SystolicArrayTest")
+            self.tt = DemoBoard.get()
+
+    TimeValue.ReBaseStringUnits = True  # I like pretty strings
+
+    runner = cocotb.get_runner(__name__)
+
+    dut = DUT()
+    dut._log.info(f"enabled factory test project.  Will test with {runner}")
+
+    runner.test(dut)
+
+    return runner
