4287d8d104479cf827bc3a9a7c1224443eceabc3
Julia Desmazes <julia.desmazes@gmail.com>
2026-07-30 16:54:55 -0500

    setup tb to test interactive lib before asic bringup, guess what ? ITS BROKEN ... because of course it is

--- test/interactive.py
+++ test/interactive.py
@@ -1,4 +1,9 @@
 
+import mac_utils 
+import cocotb 
+from cocotb.triggers import ClockCycles
+from array import array
+
 async def init(dut): 
 	# rst 
 	dut.rst_n.value = 0
@@ -19,33 +24,36 @@ async def write_weights(dut, log, W):
 async def write_data(dut, log, D) -> R:
 	log.info(f"writing data matrix {D}")
 	
-    config = bytearray(0)
-    res = array("b")
+	config = bytearray(0)
+	res = array("b")
 	b = 0
 
-    for x in X:
-        assert x >= -128 and x <= 127
-        config.append(stou(D))
+	for x in D:
+		assert x >= -128 and x <= 127
+		config.append(mac_utils.stou(x))
 
-    for i in range(0, 4):
-        dut.ui_in.value = (config[i] << 1) & 0xFE
-        uio_in = get_cmd(valid=True, mode=weight) | (config[i] >> 7 & 0x01)
-        dut.uio_in.value = uio_in
-        log.debug("write config %d:%s %s", i, config[i], uio_in)
+	for i in range(0, 4):
+		dut.ui_in.value = (config[i] << 1) & 0xFE
+		uio_in = mac_utils.get_cmd(valid=True, mode=False) | (config[i] >> 7 & 0x01)
+		dut.uio_in.value = uio_in
+		log.debug("write config %d:%s %s", i, config[i], uio_in)
 
-		if (dut.uio_out.value & 0x80 ):
-			 x = dut.uo_out.value.to_signed()
-            res.append(x) 	
+		if (dut.uio_out.value[7] == 1 ):
+			x = dut.uo_out.value.to_signed()
+			res.append(x)
 
-        await ClockCycles(dut.clk, 1)
-    dut.uio_in.value = 0
+		await ClockCycles(dut.clk, 1)
+	dut.uio_in.value = 0
 
-    while (len(res) != N * N) and (b < 100):
+	while (len(res) != 4) and (b < 100):
 		log.debug(f"reading the rest of the result, current len {len(res)}")
-        if dut.result_v.value == 1:
-            x = dut.uo_out.value.to_signed()
-            res.append(x) 	while len(rst
+		if dut.result_v.value == 1:
+			x = dut.uo_out.value.to_signed()
+			res.append(x)
+
 		b = b+1 # bail condition to prevent deadlooping
 
+	if (b == 100):
+		log.error("Timeout waiting for response")
 	log.info(f"result read {res}")
 	return res
--- test/test.py
+++ test/test.py
@@ -12,6 +12,7 @@ import jtag_utils
 import asyncio
 from array import array
 
+import interactive
 N = 2  # matrix dimention
 
 # cover full range of i8
@@ -22,244 +23,242 @@ MAX_I = 127
 
 
 def start_clk(dut):
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())  # runs the clock "in the background"
+	clock = Clock(dut.clk, 10, unit="us")
+	cocotb.start_soon(clock.start())  # runs the clock "in the background"
 
 
 def start_jtag_clk(dut):
-    jtag_clk = Clock(dut.tck, 77, unit="us")
-    cocotb.start_soon(jtag_clk.start())
+	jtag_clk = Clock(dut.tck, 77, unit="us")
+	cocotb.start_soon(jtag_clk.start())
 
 
 # Reset sequence
 async def rst(dut, ena=1, start_jtag=False):
-    dut.rst_n.value = 0
-    start_clk(dut)
-    if start_jtag:
-        start_jtag_clk(dut)
-    await ClockCycles(dut.clk, 2)
-    # set default io
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.ena.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-    dut.ena.value = ena
-    await ClockCycles(dut.clk, 10)
+	dut.rst_n.value = 0
+	start_clk(dut)
+	if start_jtag:
+		start_jtag_clk(dut)
+	await ClockCycles(dut.clk, 2)
+	# set default io
+	dut.ui_in.value = 0
+	dut.uio_in.value = 0
+	dut.ena.value = 0
+	await ClockCycles(dut.clk, 10)
+	dut.rst_n.value = 1
+	dut.ena.value = ena
+	await ClockCycles(dut.clk, 10)
 
 
 def mac(W, I):
-    res = array("b", [0, 0, 0, 0])
-    assert len(W) == N * N
-    assert len(I) == N * N
-    for x in range(0, N):
-        for y in range(0, N):
-            for ix in range(0, N):
-                tmp = res[y * N + x] + I[y * N + ix] * W[ix * N + x]
-                if tmp >= 127:
-                    tmp = 127
-                if tmp <= -128:
-                    tmp = -128
-                res[y * N + x] = tmp
-    return res
+	res = array("b", [0, 0, 0, 0])
+	assert len(W) == N * N
+	assert len(I) == N * N
+	for x in range(0, N):
+		for y in range(0, N):
+			for ix in range(0, N):
+				tmp = res[y * N + x] + I[y * N + ix] * W[ix * N + x]
+				if tmp >= 127:
+					tmp = 127
+				if tmp <= -128:
+					tmp = -128
+				res[y * N + x] = tmp
+	return res
 
 
 async def read_res(dut):
-    res = array("b")
+	res = array("b")
 
-    while len(res) != N * N:
-        if dut.result_v.value == 1:
-            x = dut.uo_out.value.to_signed()
-            res.append(x)
-        await ClockCycles(dut.clk, 1)
+	while len(res) != N * N:
+		if dut.result_v.value == 1:
+			x = dut.uo_out.value.to_signed()
+			res.append(x)
+		await ClockCycles(dut.clk, 1)
 
-    return res
+	return res
 
 
 async def compare_res(dut, W, I):
-    expected = mac(W, I)
-    res = await read_res(dut)
+	expected = mac(W, I)
+	res = await read_res(dut)
 
-    cocotb.log.info("expected vs got :")
-    cocotb.log.info(" ".join(map(str, expected)))
-    cocotb.log.info(" ".join(map(str, res)))
+	cocotb.log.info("expected vs got :")
+	cocotb.log.info(" ".join(map(str, expected)))
+	cocotb.log.info(" ".join(map(str, res)))
 
-    assert res == expected
+	assert res == expected
 
 
 # MAC tests
 
-
 @cocotb.test()
 async def simple_mac_test(dut):
-    await rst(dut)
-    W = array("b", [0, 1, 2, 3])
-    I = array("b", [4, 5, 6, 7])
+	await rst(dut)
+	W = array("b", [0, 1, 2, 3])
+	I = array("b", [4, 5, 6, 7])
 
-    await mac_utils.rst_data_addr(dut)
+	await mac_utils.rst_data_addr(dut)
 
-    # send weights
-    await mac_utils.write_config(dut, W, weight=True)
+	# send weights
+	await mac_utils.write_config(dut, W, weight=True)
 
-    # res can start comming in before all the data has been finished being written
-    comp_task = cocotb.start_soon(compare_res(dut, W, I))
+	# res can start comming in before all the data has been finished being written
+	comp_task = cocotb.start_soon(compare_res(dut, W, I))
 
-    # send data
-    write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+	# send data
+	write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
 
-    await write_task
-    await comp_task
+	await write_task
+	await comp_task
 
-    await ClockCycles(dut.clk, 10)
+	await ClockCycles(dut.clk, 10)
 
 
 @cocotb.test()
 async def random_mac_test(dut):
-    await rst(dut)
-    await mac_utils.rst_data_addr(dut)
-    for _ in range(0, 500):
-        W = array("b")
-        I = array("b")
-        for _ in range(0, 4):
-            W.append(mac_utils.biased_random(MIN_W, MAX_W))
-            I.append(mac_utils.biased_random(MIN_I, MAX_I))
+	await rst(dut)
+	await mac_utils.rst_data_addr(dut)
+	for _ in range(0, 500):
+		W = array("b")
+		I = array("b")
+		for _ in range(0, 4):
+			W.append(mac_utils.biased_random(MIN_W, MAX_W))
+			I.append(mac_utils.biased_random(MIN_I, MAX_I))
 
-        # send weights
-        await mac_utils.write_config(dut, W, weight=True)
+		# send weights
+		await mac_utils.write_config(dut, W, weight=True)
 
-        # check result - results can start streaming before all the
-        # data has been written
-        comp_task = cocotb.start_soon(compare_res(dut, W, I))
+		# check result - results can start streaming before all the
+		# data has been written
+		comp_task = cocotb.start_soon(compare_res(dut, W, I))
 
-        # send data
-        write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+		# send data
+		write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
 
-        await write_task
-        await comp_task
+		await write_task
+		await comp_task
 
 
 @cocotb.test()
 async def random_mac_reuse_weights_test(dut):
-    await rst(dut)
-    await mac_utils.rst_data_addr(dut)
-    for _ in range(0, 20):
-        W = array("b")
-        for _ in range(0, 4):
-            W.append(mac_utils.biased_random(MIN_W, MAX_W))
-        await mac_utils.write_config(dut, W, weight=True)
+	await rst(dut)
+	await mac_utils.rst_data_addr(dut)
+	for _ in range(0, 20):
+		W = array("b")
+		for _ in range(0, 4):
+			W.append(mac_utils.biased_random(MIN_W, MAX_W))
+		await mac_utils.write_config(dut, W, weight=True)
 
-        for _ in range(0, 50):
-            I = array("b")
-            for _ in range(0, 4):
-                I.append(mac_utils.biased_random(MIN_I, MAX_I))
+		for _ in range(0, 50):
+			I = array("b")
+			for _ in range(0, 4):
+				I.append(mac_utils.biased_random(MIN_I, MAX_I))
 
-            # check result - results can start streaming before all the
-            # data has been written
-            comp_task = cocotb.start_soon(compare_res(dut, W, I))
-            # write data
-            write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
+			# check result - results can start streaming before all the
+			# data has been written
+			comp_task = cocotb.start_soon(compare_res(dut, W, I))
+			# write data
+			write_task = cocotb.start_soon(mac_utils.write_config(dut, I, weight=False))
 
-            await write_task
-            await comp_task
+			await write_task
+			await comp_task
 
 @cocotb.test()
-async def interactive_mode_code(dut):
+async def interactive_mode_lib(dut):
+	await rst(dut)
 	await interactive.init(dut)
-	
-    W = array("b", [0, 1, 2, 3])
-    I = array("b", [4, 5, 6, 7])
+	W = array("b", [0, 1, 2, 3])
+	I = array("b", [4, 5, 6, 7])
 
 	await interactive.write_weights(dut, cocotb.log, W)
 	res = await interactive.write_data(dut,cocotb.log, I) 
 
-    expected = mac(W, I)
+	expected = mac(W, I)
 
-    cocotb.log.info("expected vs got :")
-    cocotb.log.info(" ".join(map(str, expected)))
-    cocotb.log.info(" ".join(map(str, res)))
+	cocotb.log.info("expected vs got :")
+	cocotb.log.info(" ".join(map(str, expected)))
+	cocotb.log.info(" ".join(map(str, res)))
 
-    assert res == expected
-
+	assert res == expected
 	
 
 # JTAG tests
 # read out idcode
 async def jtag_read_idcode(dut):
-    v, p, m = await jtag_utils.get_idcode(dut)
-    assert v == 1
-    assert p == 0xBEEF
-    assert m == 0x6B
+	v, p, m = await jtag_utils.get_idcode(dut)
+	assert v == 1
+	assert p == 0xBEEF
+	assert m == 0x6B
 
 
 # test bypass mode ( required by spec )
 async def jtag_test_bypass(dut):
-    await jtag_utils.test_bypass(dut)
+	await jtag_utils.test_bypass(dut)
 
 
 # test extest: bounday scan :) ( required by spec )
 async def jtag_extest(dut):
-    await jtag_utils.test_bsc(dut, extest=True)
+	await jtag_utils.test_bsc(dut, extest=True)
 
 
 # test sample preload: bounday scan :) ( also required by spec )
 async def jtag_sample_preload(dut):
-    await jtag_utils.test_bsc(dut, extest=False)
+	await jtag_utils.test_bsc(dut, extest=False)
 
 
 @cocotb.test()
 async def jtag_simple_test(dut):
-    await rst(dut, start_jtag=True)
-    await jtag_utils.rst_jtag_tap(dut)
-    await jtag_test_bypass(
-        dut
-    )  # jtag ir is set to idcode by default, start with bypass test to increase verification coverage
-    await jtag_read_idcode(dut)
-    await jtag_extest(dut)
-    await jtag_sample_preload(dut)
+	await rst(dut, start_jtag=True)
+	await jtag_utils.rst_jtag_tap(dut)
+	await jtag_test_bypass(
+		dut
+	)  # jtag ir is set to idcode by default, start with bypass test to increase verification coverage
+	await jtag_read_idcode(dut)
+	await jtag_extest(dut)
+	await jtag_sample_preload(dut)
 
 
 @cocotb.test()
 async def jtag_random_test(dut):
-    await rst(dut, start_jtag=True)
-    await jtag_utils.rst_jtag_tap(dut)
-    for _ in range(0, 100):
-        ir = random.randint(0, 3)
-        match ir:
-            case 0:
-                await jtag_test_bypass(
-                    dut
-                )  # jtag ir is set to idcode by default, start with bypass test to increase verification coverage
-            case 1:
-                await jtag_read_idcode(dut)
-            case 2:
-                await jtag_extest(dut)
-            case 3:
-                await jtag_sample_preload(dut)
+	await rst(dut, start_jtag=True)
+	await jtag_utils.rst_jtag_tap(dut)
+	for _ in range(0, 100):
+		ir = random.randint(0, 3)
+		match ir:
+			case 0:
+				await jtag_test_bypass(
+					dut
+				)  # jtag ir is set to idcode by default, start with bypass test to increase verification coverage
+			case 1:
+				await jtag_read_idcode(dut)
+			case 2:
+				await jtag_extest(dut)
+			case 3:
+				await jtag_sample_preload(dut)
 
 
 @cocotb.test()
 async def jtag_user_reg_test(dut):
-    await rst(dut, start_jtag=True)
-    await jtag_utils.rst_jtag_tap(dut)
+	await rst(dut, start_jtag=True)
+	await jtag_utils.rst_jtag_tap(dut)
 
-    for _ in range(0, 20):
-        W = array("b")
-        I = array("b")
-        for _ in range(0, 4):
-            W.append(mac_utils.biased_random(MIN_W, MAX_W))
-            I.append(mac_utils.biased_random(MIN_I, MAX_I))
+	for _ in range(0, 20):
+		W = array("b")
+		I = array("b")
+		for _ in range(0, 4):
+			W.append(mac_utils.biased_random(MIN_W, MAX_W))
+			I.append(mac_utils.biased_random(MIN_I, MAX_I))
 
-        # send weights
-        await mac_utils.write_config(dut, W, weight=True)
+		# send weights
+		await mac_utils.write_config(dut, W, weight=True)
 
-        for _ in range(0, 10):
-            first = True
-            unit_next = random.randint(0, 3)
+		for _ in range(0, 10):
+			first = True
+			unit_next = random.randint(0, 3)
 
-            read_reg = await jtag_utils.scan_user_reg(dut, unit_next, 0, first)
-            if not (first):
-                cocotb.log.debug("read reg %s / %s", read_reg, W[0])
-                assert read_reg == W[unit]
+			read_reg = await jtag_utils.scan_user_reg(dut, unit_next, 0, first)
+			if not (first):
+				cocotb.log.debug("read reg %s / %s", read_reg, W[0])
+				assert read_reg == W[unit]
 
-            unit = unit_next
-            first = False
+			unit = unit_next
+			first = False
